08fbd8d90f72defcbbe4a931117ff2e3fd8fbb36
Skubivi <altair100001@gmail.com>
2021-05-28 19:08:47 +0300

    Создано начальное окно

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,18 +1,22 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
+        primaryStage.setTitle("Crank Mechanism");
+        primaryStage.setScene(new Scene(root, 400, 500));
         primaryStage.show();
     }
 
